de46051ab631919aaa80b0c91c27fe717285c292
Raphaël Léger <raphael.leger@owkin.com>
2018-12-30 20:25:58 +0100

    adds some basic standardization

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
 'use strict';
 
 function frenchToSms(input) {
-    if (input === 'coucou') {
-        return 'cc';
-    }
+    let output = input;
 
-    return 'bjr';
+    output = addSpacesLeftAndRight(output);
+    output = toLowerCase(output);
+    output = standardizeApostrophes(output);
+    output = addSpaceBeforePunctuation(output);
+    output = removeAccents(output);
+
+    output = output.replace(new RegExp('coucou', 'g'), 'cc');
+    output = output.replace(new RegExp('bonjour', 'g'), 'bjr');
+    output = output.trim();
+
+    output = putPunctuationBackInPlace(output);
+
+    return output;
+}
+
+function putPunctuationBackInPlace(output) {
+    output = output.replace(/ \./g, '.');
+    output = output.replace(/ \,/g, ',');
+    output = output.replace(/ \?/g, '?');
+    output = output.replace(/ \!/g, '!');
+    output = output.replace(/  \?/g, ' ?');
+    output = output.replace(/  \!/g, ' !');
+    output = output.replace(/ \)/g, ')');
+    output = output.replace(/\( /g, '(');
+
+    return output;
+}
+
+function removeAccents(output) {
+    output = output.replace(new RegExp('î', 'g'), 'i');
+    output = output.replace(new RegExp('ô', 'g'), 'o');
+    output = output.replace(new RegExp('à', 'g'), 'a');
+    output = output.replace(new RegExp('É', 'g'), 'é');
+    output = output.replace(new RegExp('Î', 'g'), 'i');
+
+    return output;
+}
+
+function addSpaceBeforePunctuation(output) {
+    output = output.replace(/\-/g, ' -');
+    output = output.replace(/\./g, ' .');
+    output = output.replace(/\,/g, ' ,');
+    output = output.replace(/\?/g, ' ?');
+    output = output.replace(/\!/g, ' !');
+    output = output.replace(/\)/g, ' )');
+    output = output.replace(/\(/g, '( ');
+
+    return output;
+}
+
+function addSpacesLeftAndRight(output) {
+    return `  ${output}  `;
+}
+
+function toLowerCase(output) {
+    return output.toLowerCase();
+}
+
+function standardizeApostrophes(output) {
+    return output.replace(new RegExp('’', 'g'), '\'');
 }
 
 module.exports = frenchToSms;
--- test/index.js
+++ test/index.js
@@ -5,6 +5,32 @@ const fixtures = require('./fixture.json');
 const frenchToSms = require('..');
 
 
+test('caseInsensitivity', t => {
+    const expectedBonjourOutput = 'bjr';
+
+    t.equal(frenchToSms('bonjour'), expectedBonjourOutput, 'bonjour');
+    t.equal(frenchToSms('Bonjour'), expectedBonjourOutput, 'Bonjour');
+    t.equal(frenchToSms('BonJOur'), expectedBonjourOutput, 'BonJOur');
+
+    t.end()
+});
+
+
+test('punctuationInsensitive', t => {
+    t.equal(frenchToSms('bonjour'), 'bjr', 'bonjour');
+    t.equal(frenchToSms('bonjour !'), 'bjr !', 'bonjour !');
+    t.equal(frenchToSms('bonjour!'), 'bjr!', 'bonjour!');
+
+    t.end()
+});
+
+
+test('accentsInsensitive', t => {
+
+    t.end()
+});
+
+
 test('fixtures', t => {
     for (let input in fixtures) {
         const expectedOutput = fixtures[input];
