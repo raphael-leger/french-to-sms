6e7a0ef0c384ac1d0eae5c1ac4c5253d1f8d4a2f
Raphaël Léger <raphael@nodixo.com>
2021-09-15 14:08:37 +0200

    update readme

--- README.md
+++ README.md
@@ -13,6 +13,6 @@ npm install french-to-sms
 ```javascript
 const frenchToSms = require('french-to-sms');
 
-frenchToSms('coucou'); // cc
-frenchToSms('bonjour'); // bjr
+frenchToSms("coucou"); // cc
+frenchToSms("Bonjour tout le monde ! J'espère que vous allez bien ! Moi la patate !""); // bjr tt lmond ! jspr k vs allé b1 ! mwa la patate !
 ```
--- test/index.js
+++ test/index.js
@@ -49,6 +49,13 @@ test('completeSentences', t => {
 
     t.equal(
         frenchToSms(
+            `Bonjour tout le monde ! J'espère que vous allez bien ! Moi la patate !`
+        ),
+        'bjr tt lmond ! jspr k vs allé b1 ! mwa la patate !'
+    );
+
+    t.equal(
+        frenchToSms(
             `Le tofu ou fromage de soja est un aliment d'origine chinoise, issu du caillage du lait de soja. C'est une pâte blanche, molle, peu odorante et au goût plutôt neutre, constituant une base importante de l'alimentation asiatique, et aussi consommé par des végétariens et végétaliens occidentaux.`
         ),
         'ltofu ou fromaj 2 soja é 1 alimen dorigine chinoiz, issu du caillaj du lè 2 soja. cé une pate blanch, moll, pE odorante é o goût +to nutr, constituan une baz importante 2 lalimentation asiatik, é o6 consomé par D VGtari1 é VGtali1 occidento.'
